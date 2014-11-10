c943493d1aeb6c79f3782ea7d7bf1324a4a9a9ac
Silvio Jose dos Santos Junior <sjdsj@camelo.poli.br>
2014-11-09 22:25:03 -0300

    v2.0

--- ex13.c
+++ ex13.c
@@ -3,16 +3,17 @@
 #include<time.h>
 
 #define vezes 1000
-#define debug 0
+#define debug 0 
 
 int main(void)
 {
 	int vit, //numero de vitorias	
-		j,i,// variavel auxiliar
-		pri, seg; //primeira e segunda escolha do convidado
+		l,i,// variavel auxiliar
+		pri, seg, //primeira e segunda escolha do convidado
+	    cont;
 	srand(time(NULL));
 	
-	for(int cont = 0 ; cont < vezes ; cont++)
+	for(cont = 0 ; cont < vezes ; cont++)
 	{	
 		int p[3] = {0};//as portas a serem escolhidas
 		i = rand() % 3; // sorteia qual porta recebe o premio
@@ -25,12 +26,12 @@ int main(void)
 		
 		while(1) // "abre" uma porta que nao foi escolhida e que nao possui o premio
 		{
-			j = rand() % 3;
-			if(p[j] != 1 && j != pri)
+			l = rand() % 3;
+			if(p[l] != 1 && l != pri)
 				break;
 		}	
 		
-		if(debug) printf("Porta aberta = %d |\n",j );
+		if(debug) printf("Porta aberta = %d |\n",l );
 		
 		seg = pri;//a escolha da porta nao eh trocada
 		if(p[seg])// se a porta tem valor "verdadeiro" aumenta em 1 o numero de vitorias
